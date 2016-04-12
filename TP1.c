9d0830a6b06d86ede5b9319f2328bca6bf04da0a
Elise <eliseguimaraes@gmail.com>
2016-04-12 15:18:13 -0300

    Tá Pronto

--- TP1.c
+++ TP1.c
@@ -6,10 +6,7 @@
 PGM* LePGM(char* entrada) {
     FILE *arquivo;
     PGM imagem;
-    int j;
-    unsigned char dado;
-//    arquivo = fopen("exemplo_cena.pgm","w");
-//    fprintf(arquivo, "%s", "P2\n");
+    int j,i;
     arquivo = fopen(entrada, "r"); // abre o arquivo em modo de leitura
     if (arquivo == NULL)
     {
@@ -17,7 +14,7 @@ PGM* LePGM(char* entrada) {
         exit(-1);
     }
     //lê arquivo
-    char *buffer;
+    char *buffer = "";
     fscanf(arquivo, "%s", buffer); //lê a primeira linha do arquivo
     if (strcmp(buffer, "P2")) {
         puts("Arquivo com formato incorreto");
@@ -27,36 +24,47 @@ PGM* LePGM(char* entrada) {
     fscanf(arquivo, "%d", &(imagem.c));
     fscanf(arquivo, "%d", &(imagem.l));
     fscanf(arquivo, "%hhu", &(imagem.maximo));
-    // aloca os espaços na matriz
+    // aloca os espaços na matriz dinamicamente
     imagem.dados = (unsigned char**)malloc(imagem.l*sizeof(unsigned char*));
     for (j=0; j<imagem.l; j++) {
         imagem.dados[j] = (unsigned char*)malloc(imagem.c*sizeof(unsigned char));
     }
-    dado = 1;
-    printf("%d", imagem.c);
-    int i = 0;
-    for (i=0; i<imagem.c; i++) {
-        printf("\n%d", i);
+    for (i=0; i<imagem.l; i++) {
+        for (j=0; j<imagem.c; j++) {
+            fscanf(arquivo, "%hhu", &(imagem.dados[i][j]));
+        }
     }
-//    for (i=0; i<imagem.l; i++) {
-//        for (j=0; j<imagem.l; j++) {
-////            fscanf(arquivo, "%hhu", &dado);
-//            printf("%hhu\n", dado);
-////            imagem.dados[i][j] = dado;
-//        }
-//    }
     fclose(arquivo); // fecha o arquivo
 
     return &imagem;
 }
 
 int CorrelacaoCruzada(PGM *cena, PGM *obj, Ponto p) {
-    int correlacao;
+    int correlacao = 0;
+    int i, j;
+    for (i=0; i<obj->l; i++) {
+        for (j=0; j<obj->c; j++) {
+            correlacao += cena->dados[i+p.y][j+p.x]*obj->dados[i][j];
+        }
+    }
     return correlacao;
 }
 
 Ponto JanelaDeslizante(PGM *cena, PGM *obj) {
-    Ponto p;
+    Ponto p, aux;
+    int maior = 0;
+    int i,j;
+    //calcula a correlacao ao longo da cena, respeitando o limite máximo
+    for (i=0; i<cena->l - obj->l + 1; i++) {
+        for (j=0; j<cena->c - obj->c + 1; j++) {
+            aux.x = j;
+            aux.y = i;
+            if (CorrelacaoCruzada(cena, obj, p) > maior) {
+                p.x = aux.x;
+                p.y = aux.y;
+            }
+        }
+    }
     return p;
 }
 
@@ -69,8 +77,10 @@ int main(int argc, char* argv[]) {
     FILE *saida;
 
     cena = LePGM(argv[1]);
-    //objeto = LePGM(argv[2]);
-    saida = fopen(argv[3], "w"); // cria o arquivo de saída em modo de escrita
-    fclose(saida); //fecha o arquivo de saída
+    objeto = LePGM(argv[2]);
+    Ponto p = JanelaDeslizante(cena, objeto); //calcula o ponto em que a correlacao cruzada tem seu maior valor
+    saida = fopen(argv[3], "w"); // cria o arquivo de saida em modo de escrita
+    fprintf(arquivo, "%d %d", p.x, p.y);
+    fclose(saida); //fecha o arquivo de saida
     return 0;
 }
